e173c87c543fe71f2b07ade51d7d974849d89008
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-07-15 14:20:07 +0800

    Move middleware to app.module.ts

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UserController } from '../user/user.controller';
 import { UserService } from '../user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,6 +7,7 @@ import { PetOwnerModel } from '../../models/pet_owner.model';
 import { VetModel } from '../../models/vet.model';
 import { PetModel } from '../../models/pet.model';
 import { AccessTokenModel } from '../../models/access_token.model';
+import { AuthUser } from '../../middlewares/auth.middleware';
 
 @Module({
   imports: [
@@ -22,4 +23,50 @@ import { AccessTokenModel } from '../../models/access_token.model';
   providers: [UserService],
   exports: [UserService]
 })
-export class UserModule { }
+export class UserModule {
+  // configure(user: MiddlewareConsumer) {
+  //   user.apply(AuthUser).forRoutes(
+  //     {
+  //       path: 'users/register/pet-owner',
+  //       method: RequestMethod.POST,
+  //     },
+  //     {
+  //       path: 'users/register/pet-owner',
+  //       method: RequestMethod.POST,
+  //     },
+  //     {
+  //       path: 'users/vets',
+  //       method: RequestMethod.GET,
+  //     }
+  //   )
+  // }
+}
+
+
+// configure(user: MiddlewareConsumer) {
+//     user.apply(ValidateUser).forRoutes(
+//       {
+//         path: 'users/register/pet-owner',
+//         method: RequestMethod.POST,
+//       },
+//       {
+//         path: 'users/register/pet-owner',
+//         method: RequestMethod.POST,
+//       },
+//       {
+//         path: 'users/login',
+//         method: RequestMethod.POST,
+//       },
+//       {
+//         path: 'users/vets',
+//         method: RequestMethod.GET,
+//       }
+//     )
+//   }
+
+
+
+// configure(user: MiddlewareConsumer) {
+//  user.apply(ValidateUser).forRoutes(
+//       UserController
+//     )
